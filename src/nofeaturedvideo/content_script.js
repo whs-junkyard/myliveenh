c826724d79c9469d710d897d6d9bdaabdfa8795e
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2016-01-25 21:43:51 +0700

    Added nofeaturedvideo

---
+++ src/nofeaturedvideo/content_script.js
@@ -0,0 +1,29 @@
+import $ from 'jquery';
+
+let getFeaturedUser = () => {
+	return $.trim($('.recommend .margin-top-10 div:last').text());
+};
+
+let getStreamUrl = () => {
+	return $('.recommend .margin-top-10 a').attr('href');
+};
+
+let getFeaturedStreamImage = () => {
+	let cacheBust = new Date().getTime();
+	let user = getFeaturedUser();
+
+	return `http://stream.mylive.in.th/thumbnail/${user}.png?_=${cacheBust}`;
+}
+
+let generateVideoView = () => {
+	let fakeVideoView = $('<a class="body" id="enh__fakevideoview" />');
+	fakeVideoView.attr('href', getStreamUrl());
+
+	let fakeVideoViewImg = $('<img />');
+	fakeVideoViewImg.attr('src', getFeaturedStreamImage());
+	fakeVideoView.append(fakeVideoViewImg);
+
+	return fakeVideoView;
+};
+
+$('#videoview').replaceWith(generateVideoView());
