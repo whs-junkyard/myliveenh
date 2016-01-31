c8a0c27506f51df20d99db970ff834d291301dbe
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2016-01-31 17:51:26 +0700

    Added github links

--- src/settingsmenu/content_script.js
+++ src/settingsmenu/content_script.js
@@ -9,12 +9,12 @@ if(typeof window.__enh_settingsmenu === 'undefined'){
 	$('<div class="head">MyLive Enhancements</div>')
 		.appendTo(target);
 
-	$('<div class="line"><a target="_blank">ตั้งค่า</a></div>')
+	$('<div class="line"><a target="_blank"><i class="fa fa-gears" /> ตั้งค่า</a></div>')
 		.appendTo(target)
 		.find('a')
 			.attr('href', chrome.runtime.getURL(chrome.runtime.getManifest().options_page));
 
-	$('<div class="line"><a href="http://portfolio.whs.in.th/donate" target="_blank">Donate</a></div>')
+	$('<div class="line"><a href="http://portfolio.whs.in.th/donate" target="_blank"><i class="fa fa-money" /> Donate</a></div>')
 		.appendTo(target);
 
 	resume();
