62cd00fbd310bbe9ce00e07c52b26a44b425757b
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2016-01-31 17:59:28 +0700

    Better detection of user for nofeaturedvideo

--- src/nofeaturedvideo/content_script.js
+++ src/nofeaturedvideo/content_script.js
@@ -1,27 +1,27 @@
 import $ from 'jquery';
+import streamInfo from 'core/streaminfo';
 import plugin from 'core/plugin';
 
 let getFeaturedUser = () => {
 	return $.trim($('.recommend .margin-top-10 div:last').text());
 };
 
+let getThumbnailUrl = () => {
+	let user = $('.recommend .avatar').attr('src').match(/\/([^_\/]+?)_[A-Za-z0-9]+\.jpg$/)[1];
+	console.log($('.recommend .avatar').attr('src'));
+	return `http://stream.mylive.in.th/thumbnail/${user}.png`;
+};
+
 let getStreamUrl = () => {
 	return $('.recommend .margin-top-10 a').attr('href');
 };
-
-let getFeaturedStreamImage = () => {
-	let cacheBust = new Date().getTime();
-	let user = getFeaturedUser();
-
-	return `http://stream.mylive.in.th/thumbnail/${user}.png?_=${cacheBust}`;
-}
 
 let generateVideoView = () => {
 	let fakeVideoView = $('<a class="body" id="enh__fakevideoview" />');
 	fakeVideoView.attr('href', getStreamUrl());
 
 	let fakeVideoViewImg = $('<img />');
-	fakeVideoViewImg.attr('src', getFeaturedStreamImage());
+	fakeVideoViewImg.attr('src', getThumbnailUrl());
 	fakeVideoView.append(fakeVideoViewImg);
 
 	return fakeVideoView;
