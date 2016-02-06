08b9f9c7df6be18f60579bb99a1b243fd4a3220c
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2016-02-06 23:34:11 +0700

    Clickable index tag

--- src/indextags/content_script.js
+++ src/indextags/content_script.js
@@ -5,7 +5,7 @@ import plugin from 'core/plugin';
 plugin('indextags', () => {
 	$(document.body).addClass('enh__indextags');
 
-	var tagsNode = $('<div class="enh__tags"><span class="label label-default" ng-repeat="tag in room.tags" ng-bind="tag"></span></div>');
+	var tagsNode = $('<a href="/streams/[[room.no]]" class="enh__tags"><span class="label label-default" ng-repeat="tag in room.tags" ng-bind="tag"></span></a>')
 	tagsNode.insertAfter('.roomtitle');
 
 	resume();
