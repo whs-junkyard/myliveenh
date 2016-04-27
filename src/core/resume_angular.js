134b82bbf0a6da82507d8d0d5c0f7f2a01dd925f
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2016-04-28 01:14:23 +0700

    Emit connect if angular is too late for socket.io connect event

--- src/core/resume_angular.js
+++ src/core/resume_angular.js
@@ -4,5 +4,8 @@ import injectScript from 'core/injectscript';
  * Resume paused angular execution
  */
 export default function(){
-	injectScript('angular.resumeBootstrap();');
+	injectScript(`angular.resumeBootstrap();
+	if(window.socket && window.socket.connected){
+		window.socket.emit('connect');
+	}`);
 };
