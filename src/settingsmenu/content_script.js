d1ced9ee5b0859d3dcd4e9cca966e317c5615dd6
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2016-05-08 03:15:08 +0700

    Updated donation and index link

--- src/settingsmenu/content_script.js
+++ src/settingsmenu/content_script.js
@@ -7,12 +7,15 @@ plugin('settingsmenu', () => {
 	$('<div class="head">MyLive Enhancements</div>')
 		.appendTo(target);
 
-	$('<div class="line"><a target="_blank"><i class="fa fa-gears" /> ตั้งค่า</a></div>')
+	$('<div class="line"><a href="https://myliveenh.cupco.de" target="_blank" class="btn btn-link btn-sm"><span class="fa fa-globe" /> Homepage</a></div>')
+		.appendTo(target);
+
+	$('<div class="line"><a target="_blank" class="btn btn-link btn-sm"><span class="fa fa-gears" /> ตั้งค่า</a></div>')
 		.appendTo(target)
 		.find('a')
 			.attr('href', chrome.runtime.getURL(chrome.runtime.getManifest().options_page));
 
-	$('<div class="line"><a href="http://portfolio.whs.in.th/donate" target="_blank"><i class="fa fa-money" /> Donate</a></div>')
+	$('<div class="line"><a href="http://portfolio.whs.in.th/donate-th" target="_blank" class="btn btn-link btn-sm"><span class="fa fa-money" /> Donate</a></div>')
 		.appendTo(target);
 }, {
 	resume_angular: true,
